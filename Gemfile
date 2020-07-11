587d5901f6b752bc26b9510fe9b3378532f86740
andrey maysak <wondergab12@gmail.com>
2020-07-11 12:41:48 +0300

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem 'uglifier'
 gem 'coffee-rails'
 gem 'jquery-rails'
 
+gem 'bcrypt'
+
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+
+  VALID_EMAIL_REGEX = /\A[\w.+-]+@[a-z\d.-]+\.[a-z]+\z/i.freeze
+  private_constant :VALID_EMAIL_REGEX
+
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format:     { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+end
